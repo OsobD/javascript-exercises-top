90824749c8acdad724e75732e2095823b5441217
OsobD <osobdevlop@gmail.com>
2026-04-26 22:18:27 -0600

    Complete the leapYears exercise
    Implement the leapYears function using the standard three-rule leap year
    algorithm: divisible by 400 is a leap year, divisible by 100 is not,
    and divisible by 4 is. Enable all skipped tests in the test suite to
    verify correct behaviour across century and non-century years.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(year % 400 === 0){
+        return true
+    } else if(year % 100 === 0){
+        return false
+    } else if(year % 4 === 0){
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// Divisible by 400 == LeapYear
+// Divisible by 100 == noLeapYear
+// Divisible by 4   == LeapYear
